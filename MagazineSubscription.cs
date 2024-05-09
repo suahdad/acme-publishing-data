41181e8aaa25675c98258d0cee35f20971248276
jpasol <38642594+jpasol@users.noreply.github.com>
2024-05-09 12:34:34 +0800

    - bootstrap db using ef core - init ef core deps

---
+++ MagazineSubscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace acme_publishing_data;
+
+public partial class MagazineSubscription
+{
+    public string? SubscriptionId { get; set; }
+
+    public uint? MagazineId { get; set; }
+
+    public DateTime? AddedDate { get; set; }
+
+    public bool? IsActive { get; set; }
+}
